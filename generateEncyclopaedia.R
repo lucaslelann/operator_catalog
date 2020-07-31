5603fe9a0f865ce1c6cf8127a95de0db3d6a1a22
agouy <contact@agouy.com>
2020-07-31 19:52:21 +0200

    generated description

--- generateEncyclopaedia.R
+++ generateEncyclopaedia.R
@@ -3,6 +3,18 @@
 dat <- read.table("./data/operator_list.csv", header = TRUE, sep = ",")
 head(dat)
 
+dat$description <- sapply(dat$ï..name, function(x) {
+  raw <- try(readLines(paste0("https://raw.githubusercontent.com/tercen/",x,"/master/README.md")))
+  if(class(raw) == "try-error") {
+    return(NA)
+  } else {
+    desc <- try(raw[(grep("Description", raw) + 1):(grep("Usage", raw) - 1)])
+    if(class(desc) == "try-error") desc <- NA
+    return(paste0(desc, collapse=""))
+  }
+})
+
+
 unique(dat$category)
 
 chapters <- c("Basic statistics",
